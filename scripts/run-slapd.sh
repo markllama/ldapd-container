20c32e5079f6b7b2bb8838fefe4b4d50b7e9fc76
Mark Lamourine <markllama@gmail.com>
2025-04-08 15:05:36 -0400

    settle the testing init and start scripts

--- scripts/run-slapd.sh
+++ scripts/run-slapd.sh
@@ -1,17 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 DEFAULT_HOST_CONF_DIR=./test/slapd.d
 DEFAULT_HOST_DB_DIR=./test/ldap
 
 : HOST_CONF_DIR=${HOST_CONF_DIR:=${DEFAULT_HOST_CONF_DIR}}
 : HOST_DB_DIR=${HOST_DB_DIR:=${DEFAULT_HOST_DB_DIR}}
 
-mkdir -p ${HOST_CONF_DIR}
-mkdir -p ${HOST_DB_DIR}
-
-podman run --name slapd-init --rm \
+podman run --name slapd -d  \
        -v ${HOST_CONF_DIR}:/etc/openldap/slapd.d:rw,Z \
        -v ${HOST_DB_DIR}:/var/lib/ldap:rw,Z \
-       localhost/slapd \
-       /usr/sbin/slapd -T add -n0 \
-       -l /usr/share/openldap-servers/slapd.ldif \
-       -F /etc/openldap/slapd.d/
+       localhost/slapd
