9d505449b86c9e6945347ce113862acb3e89b55a
Mark Lamourine <markllama@gmail.com>
2025-04-08 15:21:55 -0400

    settle the testing init and start scripts

--- scripts/run-slapd.sh
+++ scripts/run-slapd.sh
@@ -5,7 +5,7 @@ DEFAULT_HOST_DB_DIR=./test/ldap
 : HOST_CONF_DIR=${HOST_CONF_DIR:=${DEFAULT_HOST_CONF_DIR}}
 : HOST_DB_DIR=${HOST_DB_DIR:=${DEFAULT_HOST_DB_DIR}}
 
-podman run --name slapd -d  \
+podman run --name quay.io/markllama/slapd -d  \
        --privileged --network=host --expose=389/tcp \
        -v ${HOST_CONF_DIR}:/etc/openldap/slapd.d:rw,Z \
        -v ${HOST_DB_DIR}:/var/lib/ldap:rw,Z \
