50e6e7a102162b7680ad99633152158487b8ea5f
Mark Lamourine <markllama@gmail.com>
2025-04-09 13:54:00 -0400

    fixed wrap in slapd.ldif and vars for init

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+DEFAULT_SLAPD_IMAGE=quay.io/markllama/slapd
 DEFAULT_HOST_CONF_DIR=./test/slapd.d
 DEFAULT_HOST_DB_DIR=./test/ldap
+DEFAULT_INIT_LDIF=/usr/share/openldap-servers/slapd.ldif
 
+: SLAPD_IMAGE=${SLAPD_IMAGE:=${DEFAULT_SLAPD_IMAGE}
 : HOST_CONF_DIR=${HOST_CONF_DIR:=${DEFAULT_HOST_CONF_DIR}}
 : HOST_DB_DIR=${HOST_DB_DIR:=${DEFAULT_HOST_DB_DIR}}
+: INIT_LDIF=${INIT_LDIF:=${DEFAULT_INIT_LDIF}
 
 mkdir -p ${HOST_CONF_DIR}
 mkdir -p ${HOST_DB_DIR}
@@ -13,5 +17,5 @@ podman run --name slapd-init --rm \
        -v ${HOST_DB_DIR}:/var/lib/ldap:rw,Z \
        localhost/slapd \
        /usr/sbin/slapd -T add -n0 \
-       -l /usr/share/openldap-servers/slapd.ldif \
+       -l ${INIT_LDIF} \
        -F /etc/openldap/slapd.d/
